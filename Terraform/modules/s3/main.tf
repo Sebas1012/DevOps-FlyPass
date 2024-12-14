c06d7c0e7841bdb3a84b25f2647dce094f66a108
Sebastian Henao <sebastianhenaoloaiza@gmail.com>
2024-12-14 14:11:00 -0500

    add: backend for state

--- Terraform/modules/s3/main.tf
+++ Terraform/modules/s3/main.tf
@@ -18,9 +18,7 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
         Resource = "${aws_s3_bucket.s3_bucket.arn}/*"
         Condition = {
           IpAddress = {
-            "aws:SourceIp" = [
-              var.private_subnets
-            ]
+            "aws:SourceIp" = var.private_subnets
           }
         }
       }
