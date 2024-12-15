3df5bf8543d970cd2610fac3dec8e84726e165bb
Sebastian Henao <sebastianhenaoloaiza@gmail.com>
2024-12-14 23:09:37 -0500

    cambios

--- Dockerfiles/get-ip-app/get_ip.py
+++ Dockerfiles/get-ip-app/get_ip.py
@@ -5,6 +5,6 @@ private_ip = socket.gethostbyname(socket.gethostname())
 
 print(f'Private IP: {private_ip}')
 
-with open("./ip_log.txt", "w") as file:
+with open("/data/ip_log.txt", "w") as file:
     file.write(private_ip)
 
