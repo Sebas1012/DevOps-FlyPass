8a77f3c0bcf0ff9c05517d23caaa49638135f778
Sebastian Henao <sebastianhenaoloaiza@gmail.com>
2024-12-14 20:31:51 -0500

    El final?

--- Dockerfiles/get-ip-app/get_ip.py
+++ Dockerfiles/get-ip-app/get_ip.py
@@ -5,10 +5,6 @@ private_ip = socket.gethostbyname(socket.gethostname())
 
 print(f'Private IP: {private_ip}')
 
-timestamp = str(int(time.time()))
-
-print(f'Writing IP to file: {timestamp}.txt')
-
-with open(f"./{timestamp}.txt", "w") as file:
+with open("./ip_log.txt", "w") as file:
     file.write(private_ip)
 
