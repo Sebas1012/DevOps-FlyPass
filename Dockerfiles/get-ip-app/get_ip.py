16302965c74c4e2ee29735b7a0fb867c3b804fc9
Sebastian Henao <sebastianhenaoloaiza@gmail.com>
2024-12-15 12:56:00 -0500

    fix

--- Dockerfiles/get-ip-app/get_ip.py
+++ Dockerfiles/get-ip-app/get_ip.py
@@ -1,9 +1,6 @@
 import socket
-import time
 
 private_ip = socket.gethostbyname(socket.gethostname())
-
-print(f'Private IP: {private_ip}')
 
 with open("/data/ip_log.txt", "w") as file:
     file.write(private_ip)
--- Dockerfiles/upload-s3-app/upload_s3.sh
+++ Dockerfiles/upload-s3-app/upload_s3.sh
@@ -2,7 +2,7 @@
 
 while true; do
   aws s3 cp /data/ip_log.txt s3://dev-flypass-test-s3/outputs/$(date +%s).txt
-
+  rm /data/ip_log.txt
   echo "Esperando 1 hora antes de la siguiente verificación..."
   sleep 60
 done
